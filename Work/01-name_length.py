7f07622b56a2ff9935315a573f89df551612c859
Chayatita <chayatita0409@gmail.com>
2025-06-11 10:08:07 +0700

    ส่งงานใบงาน2
    Python

---
+++ Work/01-name_length.py
@@ -0,0 +1,4 @@
+name = input("Please type your name ")
+surname = input("Please type your surname ")
+print(len(name))
+print(len(surname))
---
+++ Work/02-word_concat.py
@@ -0,0 +1,3 @@
+word_1 = input("first word:")
+word_2 = input("second word:")
+print((word_1) ," + ", (word_2))
